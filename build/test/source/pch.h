58087d22979d97775703b1f50b0b43308d407eda
mixberry <fav.mixberry@gmail.com>
2017-06-07 13:43:46 +0900

    テスト環境の変更

--- build/test/source/pch.h
+++ build/test/source/pch.h
@@ -1,27 +1,19 @@
-// stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
-// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
-// を記述します。
-//
-
 #pragma once
 
-#include "targetver.h"
+#include <SDKDDKVer.h>
 
-#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーから使用されていない部分を除外します。
-// Windows ヘッダー ファイル:
+#define WIN32_LEAN_AND_MEAN
+
 #include <windows.h>
-
-// C ランタイム ヘッダー ファイル
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
+#include <time.h>
+#include <vector>
 
-
-// TODO: プログラムに必要な追加ヘッダーをここで参照してください
 #ifdef  _DEBUG
 #include <crtdbg.h>
 #endif //_DEBUG
-#include <time.h>
-#include <vector>
+
 #include "common.h"
