f7782bc567312fe3e3824f8e9334e792859a1972
mixberry <fav.mixberry@gmail.com>
2017-06-10 17:51:40 +0900

    並列処理を改良

--- example/source/common/ParallelManager.cpp
+++ example/source/common/ParallelManager.cpp
@@ -1,10 +1,10 @@
 #include "ParallelManager.h"
 #include <process.h>
-
-//#define PARALLEL_FAKE
+#include <chrono>
 
 ParallelManager::ParallelManager() :
-	m_Init(false)
+	m_Init(false),
+	m_ThreadRotation(0)
 {
 }
 
@@ -19,45 +19,19 @@ bool ParallelManager::Initialize()
 
 	//	sysInfo.dwNumberOfProcessors = 1;
 
-#ifdef PARALLEL_FAKE
-
-	m_ThresdHandles.resize(sysInfo.dwNumberOfProcessors);
-
-#else //PARALLEL_FAKE
-
-	m_ThreadShareData.wakeupSemaphore = ::CreateSemaphore(nullptr, 0, LONG_MAX, nullptr);
-
-	::InitializeCriticalSection(&m_ThreadShareData.requestSync);
-	m_ThreadShareData.requests.reserve(sysInfo.dwNumberOfProcessors);
-
-	m_ThreadDatum.resize(sysInfo.dwNumberOfProcessors);
-	m_ThresdHandles.resize(sysInfo.dwNumberOfProcessors);
+	m_Threads.resize(sysInfo.dwNumberOfProcessors);
+	m_FunctionJobPool.resize(sysInfo.dwNumberOfProcessors);
 
 	for (uint32_t i = 0; i < sysInfo.dwNumberOfProcessors; i++)
 	{
-		DWORD mask = 1 << i;
+		m_ThreadDatum.push_back(std::make_unique<ParallelManager::THREAD_DATA>(&m_ThreadShareData, i));
 
-		m_ThreadShareData.compleateEventHandles.push_back(::CreateEvent(nullptr, FALSE, FALSE, nullptr));
+		m_Threads[i] = std::thread(ParallelManager::WorkThread, m_ThreadDatum[i].get());
 
-		m_ThreadDatum[i].pShare = &m_ThreadShareData;
-		m_ThreadDatum[i].id = i;
-
-		uintptr_t handle = ::_beginthreadex(nullptr, 0, ParallelManager::WorkThread, &m_ThreadDatum[i], CREATE_SUSPENDED, nullptr);
-		if (handle == -1)
-		{
-			return false;
-		}
-
-		m_ThresdHandles[i] = reinterpret_cast<HANDLE>(handle);
-
-		::SetThreadAffinityMask(m_ThresdHandles[i], mask);
-
-		ResumeThread(m_ThresdHandles[i]);
+		HANDLE threadHandle = reinterpret_cast<HANDLE>(m_Threads[i].native_handle());
+		uint32_t threadMask = 1 << i;
+		SetThreadAffinityMask(threadHandle, threadMask);
 	}
-
-#endif //PARALLEL_FAKE
-
-	m_FunctionJobPool.resize(sysInfo.dwNumberOfProcessors);
 
 	m_Init = true;
 
@@ -71,47 +45,48 @@ void ParallelManager::Finalize()
 		return;
 	}
 
-#ifndef PARALLEL_FAKE
-
-	// スレッドに終了を通知
 	uint32_t threadCount = GetThreadCount();
+
+	// ----------------------------------------------------------------------------------------------------
+	// スレッドに終了を通知
+	// ----------------------------------------------------------------------------------------------------
 
 	for (uint32_t i = 0; i < threadCount; i++)
 	{
-		m_ThreadShareData.requests.push_back(ParallelManager::THREAD_REQUEST(nullptr, i, i));
+		// リクストを追加
+		m_ThreadDatum[i]->requests.push_back(ParallelManager::THREAD_REQUEST(nullptr, i, i));
+
+		// スレッドを起こす
+		std::lock_guard<std::mutex> lock(m_ThreadDatum[i]->wakeupMutex);
+		m_ThreadDatum[i]->wakeup = true;
+		m_ThreadDatum[i]->wakeupCondition.notify_one();
 	}
 
-	::ReleaseSemaphore(m_ThreadShareData.wakeupSemaphore, threadCount, nullptr);
-	::WaitForMultipleObjects(static_cast<DWORD>(m_ThresdHandles.size()), m_ThresdHandles.data(), TRUE, INFINITE);
+	// ----------------------------------------------------------------------------------------------------
+	// スレッドの終了を待機
+	// ----------------------------------------------------------------------------------------------------
 
-	//解放
-	if (m_ThresdHandles.empty() == false)
+	for (uint32_t i = 0; i < threadCount; i++)
 	{
-		for (size_t i = 0; i < m_ThresdHandles.size(); i++)
-		{
-			::CloseHandle(m_ThresdHandles[i]);
-		}
-
-		m_ThresdHandles.clear();
+		m_Threads[i].join();
 	}
 
-	::DeleteCriticalSection(&m_ThreadShareData.requestSync);
-
-	if (m_ThreadShareData.compleateEventHandles.empty() == false)
-	{
-		for (size_t i = 0; i < m_ThreadShareData.compleateEventHandles.size(); i++)
-		{
-			::CloseHandle(m_ThreadShareData.compleateEventHandles[i]);
-			m_ThreadShareData.compleateEventHandles[i] = nullptr;
-		}
-		m_ThreadShareData.compleateEventHandles.clear();
-	}
-
-	::CloseHandle(m_ThreadShareData.wakeupSemaphore);
-
-#endif // PARALLEL_FAKE
+	// ----------------------------------------------------------------------------------------------------
 
 	m_Init = false;
+}
+
+void ParallelManager::Reset()
+{
+	std::vector<std::unique_ptr<THREAD_DATA>>::iterator it_begin = m_ThreadDatum.begin();
+	std::vector<std::unique_ptr<THREAD_DATA>>::iterator it_end = m_ThreadDatum.end();
+	std::vector<std::unique_ptr<THREAD_DATA>>::iterator it;
+
+	for (it = it_begin; it != it_end; ++it)
+	{
+		(*it)->info.busy = 0;
+		(*it)->info.jobCount = 0;
+	}
 }
 
 void ParallelManager::Execute(PParallelFunction function, uint32_t count, void* pData)
@@ -120,6 +95,10 @@ void ParallelManager::Execute(PParallelFunction function, uint32_t count, void* 
 	{
 		return;
 	}
+
+	// ----------------------------------------------------------------------------------------------------
+	// ジョブを追加
+	// ----------------------------------------------------------------------------------------------------
 
 	uint32_t threadCount = GetThreadCount();
 	uint32_t wakeupCount = (threadCount < count) ? threadCount : count;
@@ -134,6 +113,7 @@ void ParallelManager::Execute(PParallelFunction function, uint32_t count, void* 
 	for (uint32_t i = 0; i < wakeupCount; i++)
 	{
 		ParallelManager::ParallelFunctionJob* pSrc = &pJobs[i];
+
 		pSrc->first = batchFirst;
 		pSrc->count = batchCount;
 		pSrc->function = function;
@@ -147,72 +127,112 @@ void ParallelManager::Execute(PParallelFunction function, uint32_t count, void* 
 
 		batchFirst += pSrc->count;
 
-#ifdef PARALLEL_FAKE
-		pSrc->function(i, pSrc->first, pSrc->count, pSrc->pData);
-#else //PARALLEL_FAKE
-		m_ThreadShareData.requests.push_back(ParallelManager::THREAD_REQUEST(pSrc, i, i));
-#endif //PARALLEL_FAKE
+		uint32_t thread = (m_ThreadRotation + i) % threadCount;
+		m_ThreadDatum[thread]->requests.push_back(std::move(ParallelManager::THREAD_REQUEST(pSrc, thread, i)));
 	}
 
-#ifndef PARALLEL_FAKE
-	::ReleaseSemaphore(m_ThreadShareData.wakeupSemaphore, wakeupCount, nullptr);
-	::WaitForMultipleObjects(wakeupCount, m_ThreadShareData.compleateEventHandles.data(), TRUE, INFINITE);
-#endif //PARALLEL_FAKE
+	// ----------------------------------------------------------------------------------------------------
+	// スレッドを起こす
+	// ----------------------------------------------------------------------------------------------------
+
+	m_ThreadShareData.wakeupCount = wakeupCount;
+
+	for (uint32_t i = 0; i < wakeupCount; i++)
+	{
+		uint32_t thread = (m_ThreadRotation + i) % threadCount;
+
+		std::lock_guard<std::mutex> lock(m_ThreadDatum[thread]->wakeupMutex);
+		m_ThreadDatum[thread]->wakeup = true;
+		m_ThreadDatum[thread]->wakeupCondition.notify_one();
+	}
+
+	// ----------------------------------------------------------------------------------------------------
+	// スレッドの終了を待機
+	// ----------------------------------------------------------------------------------------------------
+
+	std::unique_lock<std::mutex> lock(m_ThreadShareData.completeMutex);
+	m_ThreadShareData.completeCondition.wait(lock, [this]() { return (m_ThreadShareData.wakeupCount == 0); });
+
+	// ----------------------------------------------------------------------------------------------------
+	// スレッドのローテーション
+	// ----------------------------------------------------------------------------------------------------
+
+	m_ThreadRotation = (m_ThreadRotation + 1) % threadCount;
 }
 
 void ParallelManager::ExecuteBatch(PParallelFunction function, uint32_t totalCount, uint32_t batchCount, void* pData)
 {
+	// ----------------------------------------------------------------------------------------------------
+	// ジョブを追加
+	// ----------------------------------------------------------------------------------------------------
+
 	uint32_t threadCount = GetThreadCount();
-	uint32_t wakeupCount = (totalCount % batchCount)? (totalCount / batchCount + 1) : (totalCount / batchCount);
+	uint32_t jobBatchCount = totalCount / batchCount;
+	uint32_t jobRemainder = totalCount % batchCount;
+	uint32_t wakeupCount = (jobBatchCount > threadCount) ? threadCount : jobBatchCount;
 
-	uint32_t compleateEventHandleCount = TO_UI32(m_ThreadShareData.compleateEventHandles.size());
-	if (compleateEventHandleCount < wakeupCount)
-	{
-		uint32_t addCount = wakeupCount - compleateEventHandleCount;
-		for (uint32_t i = 0; i < addCount; i++)
-		{
-			m_ThreadShareData.compleateEventHandles.push_back(::CreateEvent(nullptr, FALSE, FALSE, nullptr));
-		}
-	}
-
-	m_FunctionJobPool.resize(wakeupCount);
+	m_FunctionJobPool.resize(jobBatchCount);
 
 	ParallelManager::ParallelFunctionJob* pJobs = m_FunctionJobPool.data();
-	uint32_t jobCount = totalCount;
 
 	uint32_t batchFirst = 0;
 
-	for (uint32_t i = 0; i < wakeupCount; i++)
+	for (uint32_t i = 0; i < jobBatchCount; i++)
 	{
 		ParallelManager::ParallelFunctionJob* pSrc = &pJobs[i];
 		pSrc->first = batchFirst;
-		pSrc->count = (batchCount < jobCount)? batchCount : jobCount;
+		pSrc->count = batchCount + ((jobRemainder > 0)? 1 : 0);
 		pSrc->function = function;
 		pSrc->pData = pData;
 
-		if (jobCount >= batchCount)
+		if (jobRemainder > 0)
 		{
-			jobCount -= batchCount;
+			jobRemainder--;
 		}
 
 		batchFirst += pSrc->count;
 
-#ifdef PARALLEL_FAKE
-		pSrc->function(i % GetThreadCount(), pSrc->first, pSrc->count, pSrc->pData);
-#else //PARALLEL_FAKE
-		m_ThreadShareData.requests.push_back(ParallelManager::THREAD_REQUEST(pSrc, i % threadCount, i));
-#endif //PARALLEL_FAKE
+		uint32_t thread = (m_ThreadRotation + i) % threadCount;
+		m_ThreadDatum[thread]->requests.push_back(ParallelManager::THREAD_REQUEST(pSrc, thread, i));
 	}
 
-#ifndef PARALLEL_FAKE
-	::ReleaseSemaphore(m_ThreadShareData.wakeupSemaphore, wakeupCount, nullptr);
-	::WaitForMultipleObjects(wakeupCount, m_ThreadShareData.compleateEventHandles.data(), TRUE, INFINITE);
-#endif //PARALLEL_FAKE
+	// ----------------------------------------------------------------------------------------------------
+	// スレッドを起こす
+	// ----------------------------------------------------------------------------------------------------
+
+	m_ThreadShareData.wakeupCount = wakeupCount;
+
+	for (uint32_t i = 0; i < wakeupCount; i++)
+	{
+		uint32_t thread = (m_ThreadRotation + i) % threadCount;
+
+		std::lock_guard<std::mutex> lock(m_ThreadDatum[thread]->wakeupMutex);
+		m_ThreadDatum[thread]->wakeup = true;
+		m_ThreadDatum[thread]->wakeupCondition.notify_one();
+	}
+
+	// ----------------------------------------------------------------------------------------------------
+	// スレッドの終了を待機
+	// ----------------------------------------------------------------------------------------------------
+
+	std::unique_lock<std::mutex> lock(m_ThreadShareData.completeMutex);
+	m_ThreadShareData.completeCondition.wait(lock, [this]() { return (m_ThreadShareData.wakeupCount == 0); });
+
+	// ----------------------------------------------------------------------------------------------------
+	// スレッドのローテーション
+	// ----------------------------------------------------------------------------------------------------
+
+	m_ThreadRotation = (m_ThreadRotation + 1) % threadCount;
 }
 
 uint32_t ParallelManager::GetThreadCount() const
 {
-	return static_cast<uint32_t>(m_ThresdHandles.size());
+	return static_cast<uint32_t>(m_Threads.size());
+}
+
+const ParallelThreadInfo& ParallelManager::GetThreadInfo(uint32_t thread) const
+{
+	return m_ThreadDatum[thread]->info;
 }
 
 unsigned __stdcall ParallelManager::WorkThread(void* pData)
@@ -230,25 +250,55 @@ unsigned __stdcall ParallelManager::WorkThread(void* pData)
 
 	do
 	{
-		::WaitForSingleObject(pThreadShareData->wakeupSemaphore, INFINITE);
+		// ----------------------------------------------------------------------------------------------------
+		// リクエストが来るのを待機
+		// ----------------------------------------------------------------------------------------------------
+
+		{
+			std::unique_lock<std::mutex> lock(pThreadData->wakeupMutex);
+
+			pThreadData->wakeupCondition.wait(lock, [pThreadData]() { return pThreadData->wakeup; });
+			pThreadData->wakeup = false;
+		}
+
+		// ----------------------------------------------------------------------------------------------------
+		// リクエストを処理する
+		// ----------------------------------------------------------------------------------------------------
+
+		// 計測開始
+		auto startCount = std::chrono::high_resolution_clock::now();
 
 		ParallelManager::THREAD_REQUEST request(nullptr, 0, 0);
 
-		::EnterCriticalSection(&pThreadShareData->requestSync);
-
-		request = pThreadShareData->requests.back();
-		pThreadShareData->requests.pop_back();
-
-		::LeaveCriticalSection(&pThreadShareData->requestSync);
-
-		if (request.pJob != nullptr)
+		while (pThreadData->requests.empty() == false)
 		{
-			request.pJob->OnExecute(pThreadData->id, request.page);
-			SetEvent(pThreadShareData->compleateEventHandles[request.index]);
+			request = pThreadData->requests.back();
+			pThreadData->requests.pop_back();
+
+			if (request.pJob != nullptr)
+			{
+				request.pJob->OnExecute(pThreadData->id, request.page);
+				pThreadData->info.jobCount++;
+			}
+			else
+			{
+				continueLoop = false;
+			}
 		}
-		else
+
+		// 計測終了
+		auto endCount = std::chrono::high_resolution_clock::now();
+		pThreadData->info.busy += std::chrono::duration_cast<std::chrono::duration<long long, std::chrono::microseconds::period>>(endCount - startCount).count();
+
+		// ----------------------------------------------------------------------------------------------------
+		// リクエストの処理が完了したことを通知
+		// ----------------------------------------------------------------------------------------------------
+
 		{
-			continueLoop = false;
+			std::lock_guard<std::mutex> lock(pThreadShareData->completeMutex);
+
+			pThreadShareData->wakeupCount--;
+			pThreadShareData->completeCondition.notify_one();
 		}
 
 	} while (continueLoop == true);
--- example/source/common/ParallelManager.h
+++ example/source/common/ParallelManager.h
@@ -1,14 +1,22 @@
 #pragma once
 
-#include <atomic>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
 
-typedef void(__stdcall *PParallelFunction)(uint32_t thread, uint32_t page, uint32_t first, uint32_t count,	void* pData);
+typedef void(__stdcall *PParallelFunction)(uint32_t thread, uint32_t page, uint32_t first, uint32_t count,void* pData);
 
 class ParallelJob
 {
 public:
 	virtual ~ParallelJob() {}
 	virtual void OnExecute(uint32_t thread, uint32_t batch) = 0;
+};
+
+struct ParallelThreadInfo
+{
+	long long busy;
+	uint32_t jobCount;
 };
 
 class ParallelManager
@@ -20,10 +28,13 @@ public:
 	bool Initialize();
 	void Finalize();
 
+	void Reset();
+
 	void Execute(PParallelFunction function, uint32_t count, void* pData);
 	void ExecuteBatch(PParallelFunction function, uint32_t totalCount, uint32_t batchCount, void* pData);
 
 	uint32_t GetThreadCount() const;
+	const ParallelThreadInfo& GetThreadInfo(uint32_t thread) const;
 
 private:
 	class ParallelFunctionJob : public ParallelJob
@@ -38,6 +49,13 @@ private:
 		{
 			function(thread, page, first, count, pData);
 		}
+	};
+
+	struct THREAD_SHARE_DATA
+	{
+		std::mutex completeMutex;
+		std::condition_variable completeCondition;
+		uint32_t wakeupCount;
 	};
 
 	struct THREAD_REQUEST
@@ -58,27 +76,40 @@ private:
 		}
 	};
 
-	struct THREAD_SHARE_DATA
-	{
-		HANDLE wakeupSemaphore;
-		std::vector<HANDLE> compleateEventHandles;
-
-		CRITICAL_SECTION requestSync;
-		std::vector<THREAD_REQUEST> requests;
-	};
-
 	struct THREAD_DATA
 	{
 		THREAD_SHARE_DATA* pShare;
+
 		uint32_t id;
+
+		std::mutex wakeupMutex;
+		std::condition_variable wakeupCondition;
+		bool wakeup;
+
+		std::vector<THREAD_REQUEST> requests;
+
+		ParallelThreadInfo info;
+
+		THREAD_DATA(THREAD_SHARE_DATA* pShare, uint32_t id)
+		{
+			this->pShare = pShare;
+			this->id = id;
+
+			wakeup = false;
+
+			info.busy = 0;
+			info.jobCount = 0;
+		}
 	};
 
 	bool m_Init;
+
 	THREAD_SHARE_DATA m_ThreadShareData;
-	std::vector<THREAD_DATA> m_ThreadDatum;
-	std::vector<HANDLE> m_ThresdHandles;
+	std::vector<std::unique_ptr<THREAD_DATA>> m_ThreadDatum;
+	std::vector<std::thread> m_Threads;
 
 	std::vector<ParallelManager::ParallelFunctionJob> m_FunctionJobPool;
+	uint32_t m_ThreadRotation;
 
 	static unsigned __stdcall WorkThread(void* pData);
 };
